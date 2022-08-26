3bd40239c095e043dbc786ad76341a6c47ec06fd
Stas Home <staspet1988@gmail.com>
2022-08-26 15:54:51 +0300

    Small fix

--- migrations/m220823_102357_files.php
+++ migrations/m220823_102357_files.php
@@ -26,7 +26,7 @@ class m220823_102357_files extends Migration
             'share_date' => $this->bigInteger(),
             'created_at' => $this->bigInteger(),
             'updated_at' => $this->bigInteger(),
-        ));
+        ), "ENGINE=InnoDB  DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_general_ci AUTO_INCREMENT=1;)");
 
         $this->addForeignKey('fk-files-to-user', 'files', 'id_user', 'users', 'id', 'CASCADE');
         $this->addForeignKey('fk-files-to-folder', 'files', 'id_parent', 'files', 'id', 'CASCADE');
